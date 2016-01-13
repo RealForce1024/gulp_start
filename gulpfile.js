0094a6694753e76ba25faf3f72b961656c046a35
fqc <337940626@qq.com>
2016-01-13 14:30:00 +0800

    simple task defining

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,23 @@ var gulp = require('gulp'),
 //   .pipe(gulp.dest('build'))
 // });
 
-gulp.task('Task', function () {
-  return gulp.src(['js/*.js','!js/*.min.js'])
-  // return gulp.src('js/*.js') //包含min.js文件也被压缩合并进去了
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'))
-  .pipe(uglify())
-  .pipe(concat('app.js'))
-  .pipe(gulp.dest('build'))
+
+//gulp.src()
+//
+// gulp.task('Task', function () {
+//   return gulp.src(['js/*.js','!js/*.min.js'])
+//   // return gulp.src('js/*.js') //包含min.js文件也被压缩合并进去了
+//   .pipe(jshint())
+//   .pipe(jshint.reporter('default'))
+//   .pipe(uglify())
+//   .pipe(concat('app.js'))
+//   .pipe(gulp.dest('build'))
+// });
+
+
+
+
+//defining tasks
+gulp.task('gretting', function () {
+  console.log('hell world');
 });
