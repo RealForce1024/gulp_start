f68642030692856fc886c9039894e226e396f0a6
fqc <337940626@qq.com>
2016-01-13 13:59:43 +0800

    添加gulp-jshint(js语法检测插件) gulp-concat(js文件合并插件)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
-var gulp = require('gulp');
-var uglyfy = require('gulp-uglify');
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify');
+  jshint = require('gulp-jshint'),
+  concat = require('gulp-concat');
+
 // gulp.task('hello', function () {
 //   console.log("hello world");
 //
 // });
-gulp.task('minify', function () {
-  gulp.src('js/app.js')
-  .pipe(uglyfy())
+
+// gulp.task('minify', function () {
+//   gulp.src('js/app.js')
+//   .pipe(uglyfy())
+//   .pipe(gulp.dest('build'))
+// });
+
+gulp.task('Task', function () {
+  return gulp.src('js/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(uglify())
+  .pipe(concat('app.js'))
   .pipe(gulp.dest('build'))
 });
